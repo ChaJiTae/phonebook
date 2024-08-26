2ccab2aa29308c12115ada775dd1922953f5872a
지태 <wlxo402@naver.com>
2024-08-27 04:06:12 +0900

    FIN

--- src/components/ContactItem.jsx
+++ src/components/ContactItem.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import { Col, Row } from "react-bootstrap";
 import profileIcon from "../image/profileIcon.jpg";
 
-export default function ContactItem() {
+export default function ContactItem({ item }) {
   return (
     <Row>
       <Col lg={1}>
         <img width={50} src={profileIcon} />
       </Col>
       <Col lg={11}>
-        <div>차지태</div>
-        <div>010-0000-0000</div>
+        <div>{item.name}</div>
+        <div>{item.phoneNumber}</div>
       </Col>
     </Row>
   );
--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import SearchBox from "./SearchBox";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ContactItem from "./ContactItem";
+import { useSelector } from "react-redux";
 
 function ContactList() {
+  const contactList = useSelector((state) => state.contactList);
+
   return (
     <div>
       <SearchBox />
-      <ContactItem />
+      {contactList.map((item, key) => (
+        <ContactItem item={item} key={key} />
+      ))}
     </div>
   );
 }
